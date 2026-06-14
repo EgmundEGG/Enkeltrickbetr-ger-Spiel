62c578aaac4a24873c4d3bcc4b8adfe13be77fde
03ab2014 <annaboerner@freenet.de>
2026-06-14 17:56:52 +0200

    Styles lvl select view

--- enkeltrick-betrug/lvl_auswahl.gd
+++ enkeltrick-betrug/lvl_auswahl.gd
@@ -1,14 +1,16 @@
 extends Control
-@onready var exitButton = $Exit
-@onready var lvl1 = $Button
-@onready var lvl2 = $Button2
-@onready var lvl3 = $Button3
+@onready var exitButton = $Header/BackButton
+@onready var tutorial = $Content/VBoxContainer/TutorialButton
+@onready var lvl1 = $Content/VBoxContainer/GridContainer/Level1Button
+@onready var lvl2 = $Content/VBoxContainer/GridContainer/Level2Button
+@onready var lvl3 = $Content/VBoxContainer/GridContainer/Level3Button
 @onready var chat = $"../Chat"
 
 @onready var lvl_panel = $"."
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	exitButton.pressed.connect(_on_exit_pressed)
+#	tutorial.pressed.connect(on_tutorial_pressed)
 	lvl1.pressed.connect(_on_lvl1_pressed)
 	lvl2.pressed.connect(_on_lvl2_pressed)
 	lvl3.pressed.connect(_on_lvl3_pressed)
@@ -22,6 +24,11 @@ func _on_exit_pressed():
 	lvl_panel.visible = false
 	pass
 
+#func _on_tutorial_pressed():
+#	chat.lvlLoad(...)
+#	visible = false
+#	pass
+	
 func _on_lvl1_pressed():
 	chat.lvlLoad("res://lvl/lvl_0.gd")
 	visible = false
@@ -36,3 +43,4 @@ func _on_lvl3_pressed():
 	chat.lvlLoad("res://lvl/lvl_2.gd")
 	visible = false
 	pass
+	
