24b4bcde3ece117ac0fe3cb890dc97ea4b3e7c28
Erwjus <erwin.justus@mailbox.tu-dresden.de>
2026-06-14 20:14:58 +0200

    Story vor dem Level & Tutorial-Level
    Beinhaltet Story & Tutorial.
    Zusätzlich noch ein Button der ermöglicht die Story zu überspringen.

---
+++ enkeltrick-betrug/Scenes/handy.gd
@@ -0,0 +1,55 @@
+extends Node
+
+@onready var chat = $Chat
+@onready var lvl_auswahl = $LVLAuswahl
+@onready var agent_unten = $AgentUnten
+@onready var agent_oben = $AgentOben
+@onready var paiper = $Paiper
+@onready var lvl_button = $LvLButton
+@onready var main_button = $Main_Button
+
+var bereit_unten = false
+var bereit_oben = false
+var aktuelles_level = ""
+
+func _ready():
+	agent_unten.hide()
+	agent_oben.hide()
+	
+	agent_unten.tutorial_finished.connect(_on_agent_unten_fertig)
+	agent_oben.tutorial_finished.connect(_on_agent_oben_fertig)
+
+func starte_level_mit_tutorial(level_pfad: String):
+	aktuelles_level = level_pfad
+	bereit_unten = false
+	bereit_oben = false
+	
+	# Kram ausblenden
+	lvl_auswahl.hide()
+	chat.hide()
+	lvl_button.hide()
+	paiper.hide()
+	main_button.hide()
+	
+	var level_daten = load(level_pfad).new()
+	
+	agent_unten.start_dialogue(level_daten.agenten_text_unten)
+	agent_oben.start_dialogue(level_daten.agenten_text_oben)
+
+func _on_agent_unten_fertig():
+	bereit_unten = true
+	checke_start()
+
+func _on_agent_oben_fertig():
+	bereit_oben = true
+	checke_start()
+
+func checke_start():
+	if bereit_unten and bereit_oben:
+		
+		chat.show()
+		paiper.show()
+		lvl_button.show()
+		main_button.show()
+		
+		chat.lvlLoad(aktuelles_level)
